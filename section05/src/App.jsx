7c54d9b8dbe0fdbd2fa68046c789f90069be2090
SJaeYub <elapid0212@naver.com>
2025-01-26 20:24:41 +0900

    state로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,18 +1,30 @@
-// import { useState } from 'react'
+import {useState} from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header'; //react에서는 확장자를 표기하지 않아도 됨
-import Footer from './components/Footer';
-import Main from './components/Main';
-import Button from "./components/Button.jsx";
+
 
 function App() { // 부모 컴포넌트
-  // const [count, setCount] = useState(0)
-  return (
-    <>
-    </>
-  );
+    const [count, setCount] = useState(0); //state 변수 2개는 구조 분해 변수로 할당받는 것이 일반적
+    const [light, setLight] = useState("OFF");
+
+    return (
+        <>
+            <div>
+                <h1>{light}</h1>
+                <button onClick={() => setLight(light === "ON" ? "OFF" : "ON")}>
+                  {light === "ON" ? "OFF" : "ON"}
+                </button>
+            </div>
+
+            <div>
+                <h1>{count}</h1>
+                <button onClick={() => setCount(count + 1)}>
+                    +
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default App
