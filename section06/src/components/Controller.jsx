7bce6e5f752436a408214f6c021994481c95c9ea
SJaeYub <elapid0212@naver.com>
2025-01-27 09:40:31 +0900

    교재에서 개발한 카운터앱

--- section06/src/components/Controller.jsx
+++ section06/src/components/Controller.jsx
@@ -1,30 +1,30 @@
-const Controller = ({count, setCount}) => {
-    const onChange = (e) => {
-        if (e.target.value === "a") {
-            setCount(count - 1);
-        } else if (e.target.value === "b") {
-            setCount(count - 10);
-        } else if (e.target.value === "c") {
-            setCount(count - 100);
-        } else if (e.target.value === "d") {
-            setCount(count + 100);
-        } else if (e.target.value === "g") {
-            setCount(count + 10);
-        } else if (e.target.value === "f") {
-            setCount(count + 1);
-        }
-
-
-    }
-
+const Controller = ({onClickButton}) => {
     return (
         <div>
-            <button onClick={onChange} value={"a"}>-1</button>
-            <button onClick={onChange} value={"b"}>-10</button>
-            <button onClick={onChange} value={"c"}>-100</button>
-            <button onClick={onChange} value={"d"}>+100</button>
-            <button onClick={onChange} value={"g"}>+10</button>
-            <button onClick={onChange} value={"f"}>+1</button>
+            <button onClick={(() => {
+                onClickButton(-1)
+            })}>-1
+            </button>
+            <button onClick={(() => {
+                onClickButton(-10)
+            })}>-10
+            </button>
+            <button onClick={(() => {
+                onClickButton(-100)
+            })}>-100
+            </button>
+            <button onClick={(() => {
+                onClickButton(+100)
+            })}>+100
+            </button>
+            <button onClick={(() => {
+                onClickButton(+10)
+            })}>+10
+            </button>
+            <button onClick={(() => {
+                onClickButton(+1)
+            })}>+1
+            </button>
         </div>
     );
 }
